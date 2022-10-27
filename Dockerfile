ac67289446a38882464b9cf78728b071470a9bdb
densellp <densell321@gmail.com>
2022-10-26 22:19:07 -0700

    Added the ability to send JSON to requestor
    Node.js server now sends a JSON object to the requestor instead of just plain text

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,15 @@ COPY package*.json ./
 
 # install node package manager to use Node.js and server.js file
 RUN npm install
+# RUN npm install ip
+RUN npm install express
+# RUN npm install os
 
 # Copy local files into working directory in container
 COPY . .
 
 # Expose port 8080 from container to outside source
 EXPOSE 80
-CMD [ "node", "server.js" ]
+# RUN ip r
+# CMD [ "node", "server.js" ]
+CMD ip r && node server.js
--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 'use strict';
 
 const express = require('express');
+// var ip = require('ip');
+// var os = require('os');
 
-// Constants
 const PORT = 80;
 const HOST = '0.0.0.0';
+// const HOST = ip.address();
+// var test = os.networkInterfaces();
+console.log(HOST);
 
-// App
+// var date_time = Math.floor(Date.now() / 1000);
+// var json = {"message":"My name is Densell", "timestamp":date_time};
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  // res.send('Hello World');
+  res.statusCode = 200;
+  console.log("Sending JSON to requester");
+  // res.send(test);
+  res.json({
+    "message": "My name is Densell",
+    "timestamp": Math.floor(Date.now() / 1000)
+  })
 });
 
 app.listen(PORT, HOST, () => {
